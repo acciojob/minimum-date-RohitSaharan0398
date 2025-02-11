1d31480a84f14bd89c2afb8923d8116fd292bb2b
RohitSaharan0398 <rohit8607745768@gmail.com>
2025-02-11 23:23:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 function minDate(dates) {
   //write you code here
+	let n=dates.length;
+	let year="9999"
+	let month="99";
+	let date='99';
+
+	for(let i=0;i<n;i++){
+		let=y='';
+		let m='';
+		let d='';
+		for(let i=0;i<4;i++){
+			y+=dates[i];
+		}
+		for(let i=5;i<7;i++){
+			m+=dates[i];
+		}
+		for(let i=8;i<n;i++){
+			d+=dates[i];
+		}
+
+		if(year>y){
+			year=y;
+			month=m;
+			day=d;
+		}
+		else if(year==y){
+			if(month>m){
+				month=m;
+				day=d;
+			}
+			else if(month==m){
+				if(day>d){
+                    day=d;}
+			}
+		}
+		
+		
+	}
+	return year+'/'+month+'/'+day;
 }
 
 // Do not change the code
@@ -37,4 +75,4 @@ var dates = [
   "2023/03/30",
 ];
 
-alert(minDate(dates));
+//alert(minDate(dates));
